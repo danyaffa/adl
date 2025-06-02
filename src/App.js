2cc13d39386a83ac73f03f70cc5da2abb6541f13
danyaffa <leffleryd@gmail.com>
2025-06-02 11:03:42 +1000

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+```javascript
+import React from 'react';
+import ADLApp from './components/ADLApp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ADLApp />
+    </div>
+  );
+}
+
+export default App;
+```
