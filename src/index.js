38f237e678f124602a26e4fc68f194ce4bb28a5f
danyaffa <leffleryd@gmail.com>
2025-06-02 11:41:52 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
-javascript
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
+import './index.css';
 import App from './App';
-
-// Performance monitoring
-const reportWebVitals = (onPerfEntry) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <React.StrictMode>
+   <App />
+ </React.StrictMode>
 );
-
-reportWebVitals();
-
