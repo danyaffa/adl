28d376b6f8b1e4834db8dffb7289aa3f48eecaf0
danyaffa <leffleryd@gmail.com>
2025-06-03 09:49:14 +1000

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Serve static files from public directory with proper path resolution
+// Serve static files from public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // API routes
