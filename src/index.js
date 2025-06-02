06318ea570a17065223ce1887cff024c3ccd4651
danyaffa <leffleryd@gmail.com>
2025-06-03 08:25:17 +1000

    Delete src/index.js

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ADLApp from './components/ADLApp';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<ADLApp />);
